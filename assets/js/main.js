a0e2c104b3ee33f6a58164e76394d0bdae9062f9
Ted <ted_hodges@ymail.com>
2018-02-20 16:36:28 -0600

    putting together ajax

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,40 @@
      // Initial array of gifs
      var celebrityGifs = ["Bill Murray", "Leo DiCaprio", "Michael B. Jordan", "Leslie Jones"];
+
+     // Function for displaying gifs
+     function renderGifs(){
+
+        // Deleting the gif buttons prior to adding new ones
+        // Which is necessary otherwise we will repeat buttons
+        $("#en").empty();
+
+        //Looping through array of gifs
+        for (var i = 0; i < celebrityGifs.length; i++) {
+
+            // Then generate buttons for each gif in array
+            var a = $("<button>");
+            // Adding a class
+            a.addClass("")
+        }
+     }
+
+
+     //Trigger AJAX call
+$("#enterTerm").on("click", function(event){
+    //Prevent default behavior
+    event.preventDefault();
+
+    //Grab gif from input box
+    var gif = $("#enterTerm").val();
+
+    //Construct gif via URL
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=niNkgKdPVBHsWcHSnD4iSleAqH99HRaJ&limit=5";
+    
+    //Hit url with ajax
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        $("#movie-view").text(JSON.stringify(response));
+      });
+});
