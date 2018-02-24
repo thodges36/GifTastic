e7b962c566069fd443e524ac87430083a1b794eb
Ted <ted_hodges@ymail.com>
2018-02-24 12:11:45 -0600

    gif generating doesn't work so trying alternate solution

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 //Below, I try cover four main processes:
-//1. AJAX call to the API and displaying (prepend) gif
+//1. AJAX call to the API and displaying gif
 //2. Starting and stopping the gif's animation per click
 //3. Displaying buttons from the celebrityGifs array
 //4. Creating new buttons per user's form entry
@@ -25,31 +25,34 @@ $(document).ready(function () {
             .then(function (response) {
                 console.log(response)
 
+                // Storing the data from the AJAX request in the results variable, exercise 14
+                var results = response.data;
+
                 for (var j = 0; j < limit; j++); {
 
                     // Creating a div to hold the gif
-                    var gifDiv = $("<div class='gif'>");
+                    var gifDiv = $("<div class='holdGif'>");
 
                     //Creating an image for the gif
                     var gifImg = $("<img>");
                     // Fetching the image and setting up various states, see exercise 15
-                    gifImg.attr("src", response.data[j].images.original_still.url);
-                    gifImg.attr("data-still", response.data[j].images.original_still.url);
-                    gifImg.attr("data-animate", response.data[j].images.original.url);
+                    gifImg.attr("src", results[j].images.original_still.url);
+                    gifImg.attr("data-still", results[j].images.original_still.url);
+                    gifImg.attr("data-animate", results[j].images.original.url);
                     gifImg.attr("data-state", "still");
                     gifImg.attr("class", "gif");
                     //Displaying the image
                     gifDiv.append(gifImg);
 
                     // Storing the rating data
-                    var rating = response.data[j].rating;
+                    var rating = results[j].rating;
                     // Creating an element to have the rating displayed
                     var pRating = $("<p>").text("Rating: " + rating);
                     // Displaying the rating
                     gifDiv.append(pRating);
-                 
+
                     // Putting the entire gif above the previous gif
-                    $("#gifs-view").append(gifDiv);
+                    $("#gifs-view").prepend(gifDiv);
 
                 }
             });
@@ -57,8 +60,8 @@ $(document).ready(function () {
     };
 
     //Function for the gif's image state, see exercise 15
-    function stateChange(){
-        
+    function stateChange() {
+
         var state = $(this).attr("data-state");
         var animateImage = $(this).attr("data-animate");
         var stillImage = $(this).attr("data-still");
@@ -75,7 +78,6 @@ $(document).ready(function () {
     }
 
 
-
     // Function for displaying buttons
     function renderButtons() {
 
@@ -88,8 +90,9 @@ $(document).ready(function () {
 
             // Then generate buttons for each gif in array
             var a = $("<button>");
-            // Adding a class
-            a.addClass("btnInput")
+            // Adding a class and id
+            a.attr("class", "btnInput");
+            a.attr("id", "input");
             // Adding a data-attribute
             a.attr("data-name", celebrityGifs[i]);
             // Providing the initial button text
@@ -119,7 +122,7 @@ $(document).ready(function () {
     // Calling the renderButtons function to display the initial buttons
     renderButtons();
 
-    $(document).on("click", ".btnInput", displayImg);
+    $(document).on("click", "#input", displayImg);
     $(document).on("click", ".gif", stateChange);
 
 });
